cdc85a77a46f6a89ba661b7873dd5a73cee59f3e
Fábio Adler <adlerluna@hotmail.com>
2026-06-12 02:20:47 -0300

    Correção de erros, agora temos os nomes do arquivos

--- main.py
+++ main.py
@@ -58,21 +58,36 @@ def thread_download(thread_id):
 			mensagem, url, tipo = task
 			if(tipo == "video"):
 				bot.reply_to(mensagem,f"⚙️ A thread {thread_id}, está processando o seu video...")
-				nome_arquivo=f'{path_files}video_{mensagem.message_id}.mp4'
-				ydl_opts={
-					'format':'bestvideo[height<=720]+bestaudio/best[height<=720]', #Para baixar na melhor resolução possivel usar 'bestvideo[ext=mp4]+bestaudio[ext=m4a]/best[ext=mp4]/best',
-					'outtmpl':nome_arquivo,
-					'merge_output_format':'mp4'
-				}
 				print(f'thread {thread_id} processando ')
-				with yt_dlp.YoutubeDL(ydl_opts) as ydl:
-					ydl.download(url)
+				buffer_title = ""
+				with yt_dlp.YoutubeDL() as ydl:
+					info = ydl.extract_info(url,download=False)
+					for t in info.get("title","video"):
+						if (t.isalnum() or t in "_-()"):
+							buffer_title += t
+						else:
+							buffer_title += "_"
+					nome_arquivo = f"{path_files}{buffer_title}.mp4"
+					ydl_opts={
+						'format':'bestvideo[height<=720]+bestaudio/best[height<=720]', #Para baixar na melhor resolução possivel usar 'bestvideo[ext=mp4]+bestaudio[ext=m4a]/best[ext=mp4]/best',
+						'outtmpl':nome_arquivo,
+						'merge_output_format':'mp4'
+					}
+					with yt_dlp.YoutubeDL(ydl_opts) as ydl2:
+						ydl2.download(url)
 				if((os.path.getsize(nome_arquivo)/(1024*1024)) <= tam_files):
-					with open(nome_arquivo,'rb') as video_file:
-						try:
-							bot.send_video(mensagem.chat.id,video_file,caption=f'🎬 Arquvo baixado com sucesso!')
-						except:
-							bot.send_document(mensagem.chat.id,video_file,caption=f'🎬 Arquvo baixado com sucesso!')
+					while True:
+						with open(nome_arquivo,'rb') as video_file:
+							try:
+								try:
+									bot.send_video(mensagem.chat.id,video_file,caption=f'🎬 Arquvo baixado com sucesso!')
+									break
+								except:
+									bot.send_document(mensagem.chat.id,video_file,caption=f'🎬 Arquvo baixado com sucesso!')
+									break
+							except:
+								time.sleep(5)
+								pass
 				else:
 					bot.reply_to(mensagem,f"❌ Infelistmente o seu arquivo tem mais de {tam_files} MB, eu só posso mandar até {tam_files} MB")
 				if(os.path.exists(nome_arquivo)):
@@ -82,24 +97,39 @@ def thread_download(thread_id):
 				bot.reply_to(mensagem,"Ajudo em mais alguma coisa!")
 			elif(tipo == "audio"):
 				bot.reply_to(mensagem,f"⚙️ A thread {thread_id}, está processando o seu audio...")
-				nome_arquivo=f'{path_files}video_{mensagem.message_id}.mp3'
-				ydl_opts={
-					'format': 'bestaudio/best',
-					'postprocessors': [{
-						'key': 'FFmpegExtractAudio',
-						'preferredcodec': 'mp3',
-						'preferredquality': '192',
-					}],
-					'outtmpl': nome_arquivo.replace(".mp3",""),
-				}
-				with yt_dlp.YoutubeDL(ydl_opts) as ydl:
-					ydl.download(url)
+				buffer_title = ""
+				with yt_dlp.YoutubeDL() as ydl:
+					info = ydl.extract_info(url,download=False)
+					for t in info.get("title","video"):
+						if (t.isalnum() or t in "_-()"):
+							buffer_title += t
+						else:
+							buffer_title += "_"
+					nome_arquivo = f"{path_files}{buffer_title}.mp3"
+					ydl_opts={
+						'format': 'bestaudio/best',
+						'postprocessors': [{
+							'key': 'FFmpegExtractAudio',
+							'preferredcodec': 'mp3',
+							'preferredquality': '192',
+						}],
+						'outtmpl': nome_arquivo.replace(".mp3",""),
+					}
+					with yt_dlp.YoutubeDL(ydl_opts) as ydl2:
+						ydl2.download(url)
 				if((os.path.getsize(nome_arquivo)/(1024*1024)) <= tam_files):
 					with open(nome_arquivo,'rb') as audio_file:
-						try:
-							bot.send_audio(mensagem.chat.id,audio_file,caption=f'🎵 Arquvo baixado com sucesso!')
-						except:
-							bot.send_document(mensagem.chat.id,audio_file,caption=f'🎵 Arquvo baixado com sucesso!')
+						while True:
+							try:
+								try:
+									bot.send_audio(mensagem.chat.id,audio_file,caption=f'🎵 Arquvo baixado com sucesso!')
+									break
+								except:
+									bot.send_document(mensagem.chat.id,audio_file,caption=f'🎵 Arquvo baixado com sucesso!')
+									break
+							except:
+								time.sleep(5)
+								pass
 				else:
 					bot.reply_to(mensagem,f"❌ Infelistmente o seu arquivo tem mais de {tam_files} MB, eu só posso mandar até {tam_files} MB")
 				if(os.path.exists(nome_arquivo)):
@@ -107,6 +137,7 @@ def thread_download(thread_id):
 				else:
 					pass
 				bot.reply_to(mensagem,"Ajudo em mais alguma coisa!")
+				time.sleep(4)
 			else:
 				bot.reply_to(mensagem,"Tipo invaido!")
 		else:
